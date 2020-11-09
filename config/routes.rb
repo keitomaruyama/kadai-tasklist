0613b7fd170b03f5dcc90c70654eaaeaed6545a7
keito.maruyama <sumomo3427@gmail.com>
2020-11-09 06:49:08 +0000

    kadai1.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tasks
   root to: 'tasks#index'
