1a9c2762032ea2639c20f1e8777797fe8644baff
keito.maruyama <sumomo3427@gmail.com>
2020-11-09 05:52:00 +0000

    kadai1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,10 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+  
   def index
-    @tasks = Task.all
-  end
+    @tasks = current_user.tasks
+  end 
 
   def show
   end
@@ -12,15 +14,17 @@ class TasksController < ApplicationController
   end
 
   def create
-      @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
       
       if @task.save
           flash[:success] = 'タスクが正常に更新されました'
-          redirect_to @task
+          redirect_to root_url
       else
           flash.now[:danger] ='タスクは更新できませんでした'
           render :new
       end
+      
+     
   end
 
   def edit
@@ -46,13 +50,16 @@ class TasksController < ApplicationController
   end
   
   private
-  def set_task
-    @task = Task.find(params[:id])
-  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end 
+  end 
   
   def task_params
   params.require(:task).permit(:content, :status)
-  
-  end
+  end 
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tasks
   root to: 'tasks#index'
+get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  
+  
 end
