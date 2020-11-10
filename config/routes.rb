d5c1e275ebfdb8a394b4d948d5b3e8dc93e6d42d
keito.maruyama <sumomo3427@gmail.com>
2020-11-10 12:36:57 +0000

    kadai1.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :tasks
   root to: 'tasks#index'
 get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
-  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  
-  
-  
-end
+   resources :tasks
+end 
